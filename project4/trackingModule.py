5357f3b8915230dcd6a988db3233dc9222a85e31
byeongjo <gee05053@kookmin.ac.kr>
2017-11-09 12:56:47 +0900

    ss

--- project4/trackingModule.py
+++ project4/trackingModule.py
@@ -1,15 +1,15 @@
 import RPi.GPIO as GPIOA
 
 def scan():
-    self.left_most_led = 16
-    self.left_less_led = 18
-    self.center_led = 22
-    self.right_less_led = 40
-    self.right_most_led = 32
-    GPIO.setup(self.left_most_led, GPIO.IN)
-    GPIO.setup(self.left_less_led, GPIO.IN)
-    GPIO.setup(self.center_led, GPIO.IN)
-    GPIO.setup(self.right_less_led, GPIO.IN)
-    GPIO.setup(self.right_most_led, GPIO.IN)
-    return_list = [self.left_most_led, self.left_less_led, self.center_led, self.right_less_led, self.right_most_led]
+    left_most_led = 16
+    left_less_led = 18
+    center_led = 22
+    right_less_led = 40
+    right_most_led = 32
+    GPIO.setup(left_most_led, GPIO.IN)
+    GPIO.setup(left_less_led, GPIO.IN)
+    GPIO.setup(center_led, GPIO.IN)
+    GPIO.setup(right_less_led, GPIO.IN)
+    GPIO.setup(right_most_led, GPIO.IN)
+    return_list = [left_most_led, left_less_led, center_led, right_less_led, right_most_led]
     return [str(GPIO.input(x)) for x in return_list]
