5e8fb60436f86c9039835e12472d1f1ffa11b161
byeongjo <gee05053@kookmin.ac.kr>
2017-11-13 09:23:23 +0900

    setting get_tracking

--- project4_2.py
+++ project4_2.py
@@ -12,19 +12,15 @@ try :
         print(get_tracking)
         print("Distance :", get_distance)
         if get_distance > dis :
-            if get_tracking == [0,1,1,1,1] or get_tracking == [0,0,1,1,1] :
+            if get_tracking == [0,1,1,1,1] or get_tracking == [0,0,1,1,1] or get_tracking == [1,0,1,1,1] :
                 car.curve_turn("L")
-            elif get_tracking == [1,0,0,1,1] :
-                car.curve_turn("L")
-            elif get_tracking == [1,1,1,0,0] or get_tracking == [1,1,1,1,0] :
-                car.curve_turn("R")
-            elif get_tracking == [1,1,0,0,1] :
+            elif get_tracking == [1,1,1,0,0] or get_tracking == [1,1,1,1,0] or get_tracking == [1,1,1,0,1]:
                 car.curve_turn("R")
             elif get_tracking == [0,0,0,0,0] :
                 car.stop()
                 break 
             else :
-                car.run("F", 35)
+                car.run("F", 25)
             print("Obstacle :", obstacle)
         else :
             obstacle += 1
