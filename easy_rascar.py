6e3555434e49cd4dddd9e04a5df14bd9588ce0cf
Kim ByeongJo <gee05053@kookmin.ac.kr>
2017-11-13 03:29:36 +0000

    1

--- easy_rascar.py
+++ easy_rascar.py
@@ -93,7 +93,7 @@ class UltraSonicSensor:
 
     def get_distance(self):
         GPIO.output(self.trig, False)
-        time.sleep(0.001)
+        time.sleep(0.1)
         GPIO.output(self.trig, True)
         time.sleep(0.00001)
         GPIO.output(self.trig, False)
