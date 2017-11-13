6036657bca366740fd0f6fb44da21fed17877e35
byeongjo <gee05053@kookmin.ac.kr>
2017-11-13 09:16:50 +0900

    modified code

--- project4_2.py
+++ project4_2.py
@@ -14,11 +14,11 @@ try :
         if get_distance > dis :
             if get_tracking == [0,1,1,1,1] or get_tracking == [0,0,1,1,1] :
                 car.curve_turn("L")
-            elif get_tracking == [1,0,1,1,1] :
+            elif get_tracking == [1,0,0,1,1] :
                 car.curve_turn("L")
             elif get_tracking == [1,1,1,0,0] or get_tracking == [1,1,1,1,0] :
                 car.curve_turn("R")
-            elif get_tracking == [1,1,1,0,1] :
+            elif get_tracking == [1,1,0,0,1] :
                 car.curve_turn("R")
             elif get_tracking == [0,0,0,0,0] :
                 car.stop()
