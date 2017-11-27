d9367d72a6d38230c939f0256710652b760bc2df
Kim ByeongJo <gee05053@kookmin.ac.kr>
2017-11-27 00:45:31 +0000

    q

--- project5.py
+++ project5.py
@@ -9,34 +9,36 @@ try :
         print(get_tracking)
 
         # line tracing
-        if get_tracking == [1,0,0,1,1] :
-            car.curve_turn("R",20,22,-1)
-        elif get_tracking == [1,1,0,0,1] :
-            car.curve_turn("L",20,22,-1)
+        if get_tracking == [1,0,0,1,1] or get_tracking == [1,0,1,1,1] :
+            car.curve_turn("R",15,17,-1)
+        elif get_tracking == [1,1,0,0,1] or get_tracking == [1,1,1,0,1] :
+            car.curve_turn("L",15,17,-1)
 
         # right turn or stop
         elif get_tracking == [1,1,0,0,0] or get_tracking == [0,0,0,0,0]:
-            car.run("F", 35, 0.5)
+            car.run("F", 10, 0.65)
             get_tracking2 = car.trackingSensor.scan()
             if get_tracking2 == [0,0,0,0,0] : # stop
                 car.stop()
             else : # right turn
-                car.swing_turn("R", 90, 0.25)
+                car.swing_turn("R", 90, 0.3)
                 while True : # [1,1,0,1,1] 될 때 까지 회전
                     get_tracking3 = car.trackingSensor.scan()
                     if get_tracking3 == [1,1,0,1,1] or get_tracking3 == [1,0,0,1,1] or get_tracking3 == [1,1,0,0,1]:
                         break
                     else :
-                        car.swing_turn("R", 90, 0,1)
+                        car.swing_turn("R", 90, 0.1)
+                        car.stop()
+                        time.sleep(0.2)
 
         #left turn or go straight
         elif get_tracking == [0,0,0,1,1] :
-            car.run("F", 35, 0.5)
+            car.run("F", 10, 0.5)
             get_tracking2 = car.trackingSensor.scan()
             if get_tracking2 == [1,1,0,1,1] : # go straight
                 continue
             else : # left turn
-                car.swing_turn("L", 90, 0.25) # 45도 왼쪽 회전
+                car.swing_turn("L", 90, 0.3) # 45도 왼쪽 회전
                 while True :
                     get_tracking3 = car.trackingSensor.scan()
                     if get_tracking3 == [1,1,0,1,1] or get_tracking3 == [1,0,0,1,1] or get_tracking3 == [1,1,0,0,1]:
@@ -44,7 +46,7 @@ try :
                     else :
                         car.swing_turn("L", 90, 0.1)
         else :
-            car.run("F",35)
+            car.run("F",10)
 
 
 except KeyboardInterrupt :
