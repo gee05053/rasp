c3ff7fffbd967fcaad345318766c3f22cf601d85
Kim ByeongJo <gee05053@kookmin.ac.kr>
2017-11-27 00:17:06 +0000

    modified code error

--- project5.py
+++ project5.py
@@ -1,29 +1,29 @@
-from easy_rascar import Rascar, TrackingSensor
+from easy_rascar import RasCar, TrackingSensor
 import time
 
-car = Rascar()
+car = RasCar()
 
 try :
     while True :
-        get_tracking = car.TrackingSensor.scan()
+        get_tracking = car.trackingSensor.scan()
         print(get_tracking)
 
         # line tracing
         if get_tracking == [1,0,0,1,1] :
-            car.curve_turn("R",15,15,-1)
+            car.curve_turn("R",20,22,-1)
         elif get_tracking == [1,1,0,0,1] :
-            car.curve_turn("L",15,15,-1)
+            car.curve_turn("L",20,22,-1)
 
         # right turn or stop
         elif get_tracking == [1,1,0,0,0] or get_tracking == [0,0,0,0,0]:
             car.run("F", 35, 0.5)
-            get_tracking2 = car.TrackingSensor.scan()
+            get_tracking2 = car.trackingSensor.scan()
             if get_tracking2 == [0,0,0,0,0] : # stop
                 car.stop()
             else : # right turn
                 car.swing_turn("R", 90, 0.25)
                 while True : # [1,1,0,1,1] 될 때 까지 회전
-                    get_tracking3 = car.TrackingSensor.scan()
+                    get_tracking3 = car.trackingSensor.scan()
                     if get_tracking3 == [1,1,0,1,1] or get_tracking3 == [1,0,0,1,1] or get_tracking3 == [1,1,0,0,1]:
                         break
                     else :
@@ -32,13 +32,13 @@ try :
         #left turn or go straight
         elif get_tracking == [0,0,0,1,1] :
             car.run("F", 35, 0.5)
-            get_tracking2 = car.TrackingSensor.scan()
+            get_tracking2 = car.trackingSensor.scan()
             if get_tracking2 == [1,1,0,1,1] : # go straight
                 continue
             else : # left turn
                 car.swing_turn("L", 90, 0.25) # 45도 왼쪽 회전
                 while True :
-                    get_tracking3 = car.TrackingSensor.scan()
+                    get_tracking3 = car.trackingSensor.scan()
                     if get_tracking3 == [1,1,0,1,1] or get_tracking3 == [1,0,0,1,1] or get_tracking3 == [1,1,0,0,1]:
                         break
                     else :
