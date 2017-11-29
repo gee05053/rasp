59ff7b95167f2f71d9e92b63ea46e42add3dcddc
c0sm0s <c0sm0s171578@gmail.com>
2017-11-29 09:42:30 +0900

    Reformat project5 for readability

--- project5.py
+++ project5.py
@@ -3,64 +3,70 @@ import time
 
 car = RasCar()
 
-try :
-    while True :
+try:
+    while True:
         get_tracking = car.trackingSensor.scan()
         print(get_tracking)
 
         # line tracing
-        if get_tracking == [1,0,0,1,1] :
-            car.curve_turn("L",25,27,0.02)
-        elif get_tracking == [1,0,1,1,1] :
-            car.curve_turn("L",30,32,0.02)
-        elif get_tracking == [1,1,0,0,1] :
-            car.curve_turn("R",25,27,0.02)
-        elif get_tracking == [1,1,1,0,1] :
-            car.curve_turn("R",30,32,0.02)
+        if get_tracking == [1, 0, 0, 1, 1]:
+            car.curve_turn("L", 25, 27, 0.02)
+        elif get_tracking == [1, 0, 1, 1, 1]:
+            car.curve_turn("L", 30, 32, 0.02)
+        elif get_tracking == [1, 1, 0, 0, 1]:
+            car.curve_turn("R", 25, 27, 0.02)
+        elif get_tracking == [1, 1, 1, 0, 1]:
+            car.curve_turn("R", 30, 32, 0.02)
 
         # right turn or stop
-        elif get_tracking == [1,1,0,0,0] or get_tracking == [0,0,0,0,0] or get_tracking == [1,0,0,0,0] :
+        elif get_tracking == [1, 1, 0, 0, 0] or get_tracking == [0, 0, 0, 0, 0] or get_tracking == [1, 0, 0, 0, 0]:
             car.run("F", 35, 0.6)
             get_tracking2 = car.trackingSensor.scan()
-            if get_tracking2 == [0,0,0,0,0] : # stop
+            if get_tracking2 == [0, 0, 0, 0, 0]:  # stop
                 car.stop()
                 break
-            else : # right turn
+            else:  # right turn
                 car.swing_turn("R", 90, 0.2)
-                while True : # [1,1,0,1,1] 될 때 까지 회전
+                while True:  # [1,1,0,1,1] 될 때 까지 회전
                     get_tracking3 = car.trackingSensor.scan()
-                    if get_tracking3 == [1,1,0,1,1] or get_tracking3 == [1,0,0,1,1] or get_tracking3 == [1,1,0,0,1]:
+                    if get_tracking3 == [1, 1, 0, 1, 1] or get_tracking3 == [1, 0, 0, 1, 1] or get_tracking3 == [1, 1,
+                                                                                                                 0, 0,
+                                                                                                                 1]:
                         break
-                    else :
+                    else:
                         car.swing_turn("R", 90, 0.1)
                         car.stop()
                         time.sleep(0.2)
 
-        #left turn or go straight
-        elif get_tracking == [0,0,0,1,1] or get_tracking == [0,0,0,0,1]:
+        # left turn or go straight
+        elif get_tracking == [0, 0, 0, 1, 1] or get_tracking == [0, 0, 0, 0, 1]:
             car.run("F", 35, 0.5)
             get_tracking2 = car.trackingSensor.scan()
-            if get_tracking2 == [1,1,0,1,1] or get_tracking2 == [1,1,0,0,1] or get_tracking2 == [1,0,0,1,1]: # go straight
+            if get_tracking2 == [1, 1, 0, 1, 1] or get_tracking2 == [1, 1, 0, 0, 1] or get_tracking2 == [1, 0, 0, 1,
+                                                                                                         1]:  # go straight
                 continue
-            else : # left turn
-                car.swing_turn("L", 90, 0.3) # 45도 왼쪽 회전
-                while True :
+            else:  # left turn
+                car.swing_turn("L", 90, 0.3)  # 45도 왼쪽 회전
+                while True:
                     get_tracking3 = car.trackingSensor.scan()
-                    if get_tracking3 == [1,1,0,1,1] or get_tracking3 == [1,0,0,1,1] or get_tracking3 == [1,1,0,0,1]:
+                    if get_tracking3 == [1, 1, 0, 1, 1] or get_tracking3 == [1, 0, 0, 1, 1] or get_tracking3 == [1, 1,
+                                                                                                                 0, 0,
+                                                                                                                 1]:
                         break
-                    else :
+                    else:
                         car.swing_turn("L", 90, 0.1)
-        elif get_tracking == [1,1,1,1,1] :
+        elif get_tracking == [1, 1, 1, 1, 1]:
             car.swing_turn("R", 90, 0.3)
-            while True :
+            while True:
                 get_tracking2 = car.trackingSensor.scan()
-                if get_tracking2 == [1,1,0,1,1] or get_tracking2 == [1,1,0,0,1] or get_tracking2 == [1,0,0,1,1] :
+                if get_tracking2 == [1, 1, 0, 1, 1] or get_tracking2 == [1, 1, 0, 0, 1] or get_tracking2 == [1, 0, 0, 1,
+                                                                                                             1]:
                     break
-                else :
+                else:
                     car.swing_turn("R", 90, 0.1)
-        else :
-            car.run("F",35)
+        else:
+            car.run("F", 35)
 
 
-except KeyboardInterrupt :
+except KeyboardInterrupt:
     car.stop()
