0e077451a9bf02873fcd4ed08c1eefcbf837b513
c0sm0s <c0sm0s171578@gmail.com>
2017-12-08 17:25:05 +0900

    Revert try-except error handler

--- project5_2.py
+++ project5_2.py
@@ -1,4 +1,5 @@
 from easy_rascar import RasCarUsingTrackingSensor
+import RPi.GPIO as GPIO
 
 
 def rotates_until_reaches_black_line(way_to_turn, direction, speed, running_time):
@@ -25,40 +26,43 @@ def rotates_until_reaches_black_line(way_to_turn, direction, speed, running_time
 
 
 car = RasCarUsingTrackingSensor(0, 0)
+try:
+    while True:
+        tracking_status = car.trackingSensor.scan()
+        print(tracking_status)
 
-while True:
-    tracking_status = car.trackingSensor.scan()
-    print(tracking_status)
+        # Line Tracing
+        if tracking_status == [1, 1, 0, 1, 1]:
+            car.run("F", 40, -1)
+        if tracking_status == [1, 0, 0, 1, 1]:
+            car.curve_turn("L", 30, 35, -1)
+        elif tracking_status == [1, 1, 0, 0, 1]:
+            car.curve_turn("R", 30, 35, -1)
+        elif tracking_status == [1, 0, 1, 1, 1]:
+            car.curve_turn("L", 30, 50, -1)
+        elif tracking_status == [1, 1, 1, 0, 1]:
+            car.curve_turn("R", 30, 50, -1)
 
-    # Line Tracing
-    if tracking_status == [1, 1, 0, 1, 1]:
-        car.run("F", 40, -1)
-    if tracking_status == [1, 0, 0, 1, 1]:
-        car.curve_turn("L", 30, 35, -1)
-    elif tracking_status == [1, 1, 0, 0, 1]:
-        car.curve_turn("R", 30, 35, -1)
-    elif tracking_status == [1, 0, 1, 1, 1]:
-        car.curve_turn("L", 30, 50, -1)
-    elif tracking_status == [1, 1, 1, 0, 1]:
-        car.curve_turn("R", 30, 50, -1)
-
-    # if the car detects 0 at the rightmost sensor
-    elif tracking_status[4] == 0:
-        car.run("F", 30, 0.26)
-        # if the car arrives at the finishing line
-        if car.trackingSensor.scan() == [0, 0, 0, 0, 0]:
-            break
+        # if the car detects 0 at the rightmost sensor
+        elif tracking_status[4] == 0:
+            car.run("F", 30, 0.26)
+            # if the car arrives at the finishing line
+            if car.trackingSensor.scan() == [0, 0, 0, 0, 0]:
+                break
+            else:
+                car.swing_turn("R", 90, 0.25)
+                rotates_until_reaches_black_line("swing", "R", 70, -1)
+        elif tracking_status == [1, 1, 1, 1, 1]:
+            car.run("F", 30, 0.4)
+            rotates_until_reaches_black_line("point", "L", 45, -1)
+        elif tracking_status == [0, 0, 0, 1, 1] or tracking_status == [0, 1, 0, 1, 1]:
+            car.run("F", 30, 0.2)
+            if car.trackingSensor.scan() == [1, 1, 1, 1, 1]:
+                rotates_until_reaches_black_line("swing", "L", 45, -1)
+            else:
+                continue
         else:
-            car.swing_turn("R", 90, 0.25)
-            rotates_until_reaches_black_line("swing", "R", 70, -1)
-    elif tracking_status == [1, 1, 1, 1, 1]:
-        car.run("F", 30, 0.4)
-        rotates_until_reaches_black_line("point", "L", 45, -1)
-    elif tracking_status == [0, 0, 0, 1, 1] or tracking_status == [0, 1, 0, 1, 1]:
-        car.run("F", 30, 0.2)
-        if car.trackingSensor.scan() == [1, 1, 1, 1, 1]:
-            rotates_until_reaches_black_line("swing", "L", 45, -1)
-        else:
-            continue
-    else:
-        car.run("F", 20)
+            car.run("F", 20)
+except KeyboardInterrupt:
+    car.stop()
+    GPIO.cleanup()
